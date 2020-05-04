877686a7a4c069ff1288cc30e6a8611740ab953e
Rawaa Al Kabbani <rawaa.alkabbani@outlook.com>
2020-05-04 19:22:52 +0200

    Use a session for the login.

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const data = require('./data.js');
 const express = require('express');
+const session = require('express-session');
 const path = require('path');
+require('dotenv').config();
 const port = 2000;
 const app = express();
 const indexFilePath = path.join(__dirname, 'public', 'index.html');
+
+app.use(
+  session({
+    secret: process.env.Session_Secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,22 +22,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/getProjects', (request, response) => {
-  data.getProjects().then(projects => {
+  data.getProjects().then((projects) => {
     response.send(projects);
     response.end();
   });
 });
 
-app.post('/createProject', (request, response) => {
+function requireAdmin(request, response, next) {
+  if (request.session.isAdmin) {
+    return next();
+  }
+  response.status(403).end();
+}
+
+app.post('/createProject', requireAdmin, (request, response) => {
   const body = request.body;
   data.saveProject(body.title, body.about, body.link);
   response.send(body.title);
   response.end();
 });
+
 app.post('/verifyUser', (request, response) => {
   const body = request.body;
   let res = data.verifyUser(body.user, body.password);
   if (res === 1) {
+    request.session.isAdmin = true;
     response.redirect('/editProject');
   } else {
     response.redirect('/admin');
